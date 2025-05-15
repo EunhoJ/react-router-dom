0d1f9e52e59a47a0b34941e56153f9eaa6569426
Eunhoj <john.vieira99@gmail.com>
2025-05-15 16:01:43 -0300

    Css Global

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import { Navigation } from "./Routes/Navigation";
 import { AppRoutes } from "./Routes/AppRoutes";
+
 
 export function App() {
   return (
     <Router>
-        <Navigation />
         <AppRoutes />
     </Router>
   );
