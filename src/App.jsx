889c18bb7936bdf476ff6bb3727ad27b5685684f
Eunhoj <john.vieira99@gmail.com>
2025-05-15 15:33:06 -0300

    Updates

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import { Navigation } from "./Routes/Navigation";
+import { AppRoutes } from "./Routes/AppRoutes";
+
+export function App() {
+  return (
+    <Router>
+        <Navigation />
+        <AppRoutes />
+    </Router>
+  );
+}
---
+++ src/Pages/About.jsx
@@ -0,0 +1,9 @@
+
+export function About() {
+  return (
+    <>
+      <h1>Sobre a empresa</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque, labore!</p>
+    </>
+  );
+}
